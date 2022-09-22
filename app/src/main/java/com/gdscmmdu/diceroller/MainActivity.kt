d531986e6551eeedda2f81ad16fa59e480b03ea8
sushant102004 <sushant.dhiman9812@gmail.com>
2022-09-22 07:23:17 +0530

    Project Completed

---
+++ app/src/main/java/com/gdscmmdu/diceroller/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.gdscmmdu.diceroller
+
+import android.os.Bundle
+import android.util.Log
+import android.util.Log.DEBUG
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.gdscmmdu.diceroller.ui.theme.DiceRollerTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceAndButton()
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceAndButton() {
+    var result by remember { mutableStateOf(1)}
+
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(onClick = {
+            result = (1..6).random()
+
+        }) {
+            Text(stringResource(R.string.rollButtonText))
+        }
+    }
+}
